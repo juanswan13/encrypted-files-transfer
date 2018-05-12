b1059e5cf4c3a513e75eb8f759e352ec4e6e8d52
Juan Camilo Swan <juanswan13@hotmail.com>
2018-05-12 18:58:40 -0500

    Creación de paquetes

---
+++ encrypted-files-transfer/src/Vista/InterfazProyecto.java
@@ -0,0 +1,17 @@
+package Vista;
+
+import javax.swing.JFrame;
+
+
+
+public class InterfazProyecto extends JFrame {
+	private static final long serialVersionUID = 10L;
+	
+	
+
+
+	public static void main(String[] args) {
+		InterfazProyecto ventana = new InterfazProyecto();
+		ventana.setVisible(true);
+	}
+}
